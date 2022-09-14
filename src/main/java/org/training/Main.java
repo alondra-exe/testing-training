c1e3602849bd9232fad0d4e61bd8a66b209415a7
alondra-exe <aeli_ds@hotmail.com>
2022-09-14 10:48:24 -0500

    Change from release branch :)

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -3,5 +3,6 @@ package org.training;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Change made on release branch.");
     }
 }
